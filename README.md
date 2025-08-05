069c27677227f7405155e8bb5fc4bb30ee14db78
Qizhong Li <s215125@dtu.dk>
2025-08-05 14:33:47 +0200

    Added some stuff to README and 2 qubit operations in the main file

---
+++ README.md
@@ -0,0 +1,5 @@
+I made this repo pulbic because my professor wanted this exercise to be public. Not sure why anyone would look at this but ok.
+
+Main.py contains some basic definitions, such as one and two qubit gates. From there, anyone should be able to calculate any circuit they want by using a combination of kronecker products (np.kron) and matrix multiplications. I originally had these stuff on a Maple file so for this exercise, I basically moved everything from there to a Python file instead. I'll add some more stuff that I think would be useful tools and see where it goes from there.
+
+If someone is really inclined on using a quantum circuit simulator, you're probably better off using quirk instead.
--- main.py
+++ main.py
@@ -9,6 +9,35 @@ H = np.array([[1, 1], [1, -1]]) / np.sqrt(2)  # Hadamard gate
 X = np.array([[0, 1], [1, 0]])  # Pauli-X gate
 Z = np.array([[1, 0], [0, -1]])  # Pauli-Z gate
 Y = np.array([[0, -1j], [1j, 0]])  # Pauli-Y gate
+T = np.array([[1, 0], [0, np.exp(1j * np.pi / 4)]])  # T gate
+S = np.array([[1, 0], [0, 1j]])  # S gate
+I = np.array([[1, 0], [0, 1]])  # Identity gate
+
+# So running a circuit with H, T, X with a qubit initialized to |0> will result in:
+print(X @ T @ H @ bit0)
 
 
+### 2 qubit 
+zerozero = np.kron(bit0, bit0)  # |00>
+zerone = np.kron(bit0, bit1)  # |01>
+onezero = np.kron(bit1, bit0)  # |10>
+oneone = np.kron(bit1, bit1)  # |11>
 
+# Defining some two qubit gates
+CNOT = np.array([[1, 0, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 1],
+                 [0, 0, 1, 0]])  # CNOT gate. Control is the first qubit (bottom), target is the second qubit (top)
+
+CNOT2 = np.array([[1, 0, 0, 0],
+                  [0, 0, 0, 1],
+                  [0, 0, 1, 0],
+                  [0, 1, 0, 0]])  # CNOT gate with control and target swapped
+
+CZ = np.array([[1, 0, 0, 0],
+               [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, -1]])  # Controlled-Z gate
+
+# If I apply H on both qubits and a CNOT gate, I will get the following state:
+print(CNOT @ (np.kron(H @ bit0, H @ bit0)))
