91ed1c6ffc52a4cbdbc130cae540bf6b06989a05
Qizhong Li <s215125@dtu.dk>
2025-08-05 14:03:27 +0200

    Changed some stuff

--- main.py
+++ main.py
@@ -5,19 +5,10 @@ bit0 = np.array([1, 0])  # |0>
 bit1 = np.array([0, 1])  # |1>
 
 # Definind some single qubit gates
-def hadamard(bit):
-    """Hadamard gate"""
-    return (1/np.sqrt(2)) * np.array([[1, 1], [1, -1]]).dot(bit)
+H = np.array([[1, 1], [1, -1]]) / np.sqrt(2)  # Hadamard gate
+X = np.array([[0, 1], [1, 0]])  # Pauli-X gate
+Z = np.array([[1, 0], [0, -1]])  # Pauli-Z gate
+Y = np.array([[0, -1j], [1j, 0]])  # Pauli-Y gate
 
-def pauli_x(bit):
-    """Pauli-X gate (bit flip)"""
-    return np.array([[0, 1], [1, 0]]).dot(bit)
 
-def pauli_y(bit):
-    """Pauli-Y gate (bit flip and phase flip)"""
-    return np.array([[0, -1j], [1j, 0]]).dot(bit)
-
-def pauli_z(bit):
-    """Pauli-Z gate (phase flip)"""
-    return np.array([[1, 0], [0, -1]]).dot(bit)
 
